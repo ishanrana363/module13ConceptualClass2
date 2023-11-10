0d2dfcfcfff57c071b41822851ed618152553b24
ishanrana363 <ishanrana363@gmail.com>
2023-11-10 20:17:20 +0600

    password toggle done

--- src/Component/login-From/Loginform.jsx
+++ src/Component/login-From/Loginform.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react"
 import "./loginForm.css"
+import { FaToggleOn } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 const Loginform = () => {
+  const [togglePass,setTogglePss] = useState(true)
+  const passwordVisibility = () =>{
+    setTogglePss(!togglePass)
+  }
+  const handleFrom = (e) =>{
+    e.preventDefault()
+  }
   return (
-    <div className="from-container" >
-      <form >
-        {/* User name input */}
+    <div className='from-container' >
+      <form action="" onSubmit={handleFrom} >
         <div>
-            <span>Username : </span>
-            <input type="text" />
+          <label htmlFor="username">UserName : </label>
+          <input type="text" name="" id="username" />
         </div>
-        {/* User password input */}
         <div>
-            <span>Password : </span>
-            <input  type="password" name="" id="password" />
+          <label htmlFor="password">Password : </label>
+          <input className="password" 
+          type= {togglePass? "password" : "text" } name="" id="password"/>
+          <button className="pssvisibility" onClick={passwordVisibility} >
+            {
+              togglePass ? <FaToggleOn/> : <FaTimes/>
+            }
+          </button>
+        </div>
+        <div className="btn" >
+          <button  type='submit' >Login</button>
         </div>
       </form>
     </div>
